ad84be9c5f84bb7b7814e4f65533d9f960dcd5a2
DmitryT24 <t.tdn@yandex.ru>
2024-02-05 21:38:24 +0300

    Добавьте файлы проекта.

---
+++ First_Modul324/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Modul324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Jane";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
